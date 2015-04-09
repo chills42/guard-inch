87c901db0985c8cde23c5c47f17c21df10b6900b
Craig Hills <chills@gmail.com>
2015-04-08 20:44:21 -0400

    Improve documentation (ironically)

--- lib/guard/inch.rb
+++ lib/guard/inch.rb
@@ -2,8 +2,12 @@ require "guard/inch/version"
 require 'guard'
 require 'guard/plugin'
 
+# The guard namespace
 module Guard
+  # A guard plugin for the Inch documentation lint tool
   class Inch < Plugin
+    # configure a new instance of the plugin
+    # @param [Hash] options the guard plugin options
     def initialize(options = {})
       super
       @options = options
@@ -11,6 +15,7 @@ module Guard
       @all_type = options[:all_type] || :none
     end
 
+    # On start, display a message and optionally run the documentation lint
     def start
       message = 'Guard::Inch is running'
       message << ' in pedantic mode' if options[:pedantic]
@@ -18,6 +23,7 @@ module Guard
       run_all if options[:all_on_start]
     end
 
+    # Run all of the documentation lints
     def run_all
       return unless options[:all_type]
       args = case options[:all_type]
@@ -31,12 +37,15 @@ module Guard
       args << ' .'
       run_inch args
     end
+
+    # Run the lints for changes files
+    # @param [Array<String>] paths the paths of changed files
     def run_on_changes(paths)
       flags = ''
       flags << '--pedantic ' if options[:pedantic]
       run_inch "#{flags} #{paths.join(' ')}"
     end
-    
+
     private
 
     # run the inch tool with the specified argument string
--- lib/guard/inch/version.rb
+++ lib/guard/inch/version.rb
@@ -1,5 +1,7 @@
+# The guard namespace
 module Guard
+  # The Inch Plugin Version namespace
   module InchVersion
-    VERSION = "0.1.0"
+    VERSION = '0.1.1'
   end
 end
